bd0d5db732137b3cbae8ce5596770832b838333a
dcannonwalker <42654459+dcannonwalker@users.noreply.github.com>
2026-02-04 13:15:35 -0600

    get params step

--- r/simulation.R
+++ r/simulation.R
@@ -1,46 +1,30 @@
 # get data set
 data(kidney, package = 'SimSeq')
 # extract data set parameters
-counts=kidney$counts # RNA-seq read count data
-index.cancer=(1:72)*2 # cancer sample index
-index.normal=index.cancer-1 # normal sample index
-counts= counts[,c(index.cancer, index.normal)] # Arrange samples for convinience
-mean.total = apply(counts,1,mean)
-index.filter = which(mean.total < 10)
-counts <- counts[!index.filter, ]
 .get_params <- function(counts, group) {
-  dge=DGEList(counts=counts, group = group)
-  dge=calcNormFactors(dge)
-  dge=estimateCommonDisp(dge)
-  dge=estimateTagwiseDisp(dge)
-  disp = dge$tagwise.dispersion # Dispersion
-  mean = apply(counts,1,mean)
+  dge <- edgeR::DGEList(counts = counts, group = group)
+  dge <- edgeR::calcNormFactors(dge)
+  dge <- edgeR::estimateCommonDisp(dge)
+  dge <- edgeR::estimateTagwiseDisp(dge)
+  disp <- dge$tagwise.dispersion # Dispersion
+  mean <- apply(counts,1,mean)
   list(disp = disp, mean = mean)
 }
-# Get count mean and dispersion using edgeR package
+#' counts: a matrix of counts
+#' groups: a vector giving treatment groups
+get_params <- function(counts, groups, mean_count_filter = 10) {
+  mean_total = apply(counts,1,mean)
+  index_filter = which(mean_total < mean_count_filter)
+  counts <- counts[index_filter, ]
+  params <- list()
+  for (group_name in unique(groups)) {
+    params[[group_name]] <- .get_params(counts = counts[, groups == group_name], 
+                                        group = factor(groups[groups == group_name]))
+  }
+  params[["total"]] <- .get_params(counts = counts, group = factor(groups))
+  params
+}
 
-# Mean and dispersion values are obtained separately from cancer and normal samples when different dispersion is assummed between two sample types.
-# Mean and dispersion values from normal samples
-params.normal <- .get_params(counts = counts[, 73:144], group = factor(rep(2, 72)))
-# Mean and dispersion values from cancer samples
-params.cancer <- .get_params(counts = counts[, 1:72], group = factor(rep(1, 72)))
+kparams <- get_params(counts = kidney$counts, groups = kidney$treatment)
 
-# Gene filtering: Genes having small read count (<10) are filtered
-#### edgeR would recommend doing this filtering BEFORE fitting the models - 
-#### also much nicer code to do it that way???
-mean.total = apply(counts,1,mean)
-index.filter = which(mean.total < 10)
-mean.total = mean.total[-index.filter]
-disp.normal = disp.normal[-index.filter]
-disp.cancer = disp.cancer[-index.filter]
-mean.normal = mean.normal[-index.filter]
-mean.cancer = mean.cancer[-index.filter]
-
-
-# Mean and dispersion values obtained using all samples when same dispersion is assummed between two sample types..
-dge.total = DGEList(counts = counts, group = factor(c(rep(1,72),rep(2,72))))
-dge.total = calcNormFactors(dge.total)
-dge.total = estimateCommonDisp(dge.total)
-dge.total = estimateTagwiseDisp(dge.total)
-disp.total = dge.total$tagwise.dispersion
-disp.total = disp.total[-index.filter]
+# 
